8daff19a8ccc7dedd4b48f21eff7ae38f343f233
stefanivladimirova <stefivs21@gmail.com>
2020-04-26 18:34:11 +0300

    Adding Jurassic.h

---
+++ Jurassic.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "Cage.h"
+class Jurassic
+{
+	size_t fSize_cage;
+	size_t fCapacity_cage;
+
+	size_t fSize_dinos;
+	size_t fCapacity_dinos;
+
+	Cage* fCages;
+	Dinosaur* fDinos;
+
+	void resize_cage(size_t newCapacity_cage);
+	void resize_dinos(size_t newCapacity_dinos);
+
+public:
+	void add_cage(const Cage& newCage);
+	void add_dinosaur(const Dinosaur& newDinos);
+
+	void remove();
+
+	void add_food();
+
+	Jurassic();
+	Jurassic(size_t, size_t, size_t, size_t);
+	void initialize(size_t, size_t, size_t, size_t);
+
+	Jurassic(const Jurassic& other);
+	Jurassic& operator=(const Jurassic& other);
+
+	void copyFrom_Jurassic(const Jurassic& other);
+	~Jurassic();
+
+	void print_Jurassic()const;
+	void readFromConsole();
+
+	//validates if the dinos are from the same era
+	bool valid_dino();
+
+	//validates if the dinos are from the same order
+	bool valid_dino2();
+
+	//check if the dino/dinos can live in a particular climate cage
+	//dedided to make them in different methods cause it is easier for me
+	//the first one checks when the climate is "Terrestrial"
+	bool check_climate();
+
+	//the next one checks if the climate is "Aqueous"
+	bool check_climate_aq();
+
+	//the last one checks if the climate is "Aerial"
+	bool check_climate_ae();
+};
